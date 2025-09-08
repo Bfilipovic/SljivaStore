dd000f7a28b7d1606bdef616448791e524e32216
Bratislav Filipovic <bratislavfilipovic99@gmail.com>
2025-09-08 22:14:14 +0200

    Fix navigation

--- frontend/src/lib/Nav.svelte
+++ frontend/src/lib/Nav.svelte
@@ -11,10 +11,26 @@
 
 <!-- top navigation bar -->
 <div class="bg-gray-900 text-white p-4">
-  <div class="flex justify-center space-x-6">
-    <a href="/" class="hover:underline">STORE</a>
-    <a href="/selling" class="hover:underline">MY NFTS</a>
-    <a href="/mint" class="hover:underline">MINT</a>
+  <div class="flex justify-between items-center max-w-4xl mx-auto">
+    <!-- left: nav links -->
+    <div class="flex space-x-6">
+      <a href="/" class="hover:underline">STORE</a>
+      <a href="/selling" class="hover:underline">MY NFTS</a>
+      <a href="/mint" class="hover:underline">MINT</a>
+    </div>
+
+    <!-- right: login/logout -->
+    {#if $walletAddress}
+      <button
+        on:click={logout}
+        class="bg-gray-600 hover:bg-gray-700 text-white px-3 py-1"
+      >Logout</button>
+    {:else}
+      <a
+        href="/login"
+        class="bg-gray-600 hover:bg-gray-700 text-white px-3 py-1"
+      >Login</a>
+    {/if}
   </div>
 </div>
 
@@ -22,8 +38,8 @@
 {#if $walletAddress}
   <div class="bg-gray-800 text-white px-4 py-2">
     <div class="flex items-center justify-between max-w-4xl mx-auto">
-      <div class="text-sm text-gray-300">
-        <div class="font-mono">{$shortAddress}</div>
+      <div class="text-sm text-gray-300 break-words max-w-full">
+        <div class="font-mono break-all">{$walletAddress}</div>
         <div class="text-xs">Balance: {$walletBalance} ETH</div>
         {#if $walletGifts.length > 0}
           <div class="mt-1 text-yellow-400 text-xs">
@@ -32,10 +48,6 @@
           </div>
         {/if}
       </div>
-      <button
-        on:click={logout}
-        class="bg-gray-600 hover:bg-gray-700 text-white px-3 py-1"
-      >Logout</button>
     </div>
   </div>
 {/if}
